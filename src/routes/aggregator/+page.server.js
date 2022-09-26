535f6ae02fddc67b6003e5ab79752e8b7ad39ef8
Thor Anderson <ta.anderson@gmail.com>
2022-09-26 14:27:23 -0600

    Trying out Hacker news API

--- src/routes/aggregator/+page.server.js
+++ src/routes/aggregator/+page.server.js
@@ -1,14 +1,44 @@
 import { NEWS_APIKEY } from '$env/static/private'
 import { error } from '@sveltejs/kit'
 
-//https://api.thenewsapi.com/v1/news/headlines?locale=us&language=en&api_token=YOUR_API_TOKEN
+//https://api.thenewsapi.com/v1/news/top?locale=us&language=en&api_token=YOUR_API_TOKEN
 
 export async function load() {
     try {
-        const news_response = await fetch(`https://api.thenewsapi.com/v1/news/top?locale=us&language=en&api_token=${NEWS_APIKEY}`)
+        const news_response = await fetch(`https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`)
         const news_data = await news_response.json()
-        return { newsData: news_data.data}
+        const allTopStories = await getAllTopStories(news_data)
+        return allTopStories
     } catch (err) {
         throw error(404, 'Not found')
     }
+}
+
+async function getAllTopStories(topStoryIDArray) {
+    let allTopStories = []
+
+    //console.log(topStoryIDArray.length)
+    /* 
+    if(totalResults === 0) return // should probably let user know "No results"
+    if (totalResults > 0 && totalResults < 11) {
+        return pageOne.Search // return just the array of the results
+    }
+    allTheMovies = [...pageOne.Search]
+    // figure out how many times to loop
+    let counter = Math.ceil(totalResults / 10)
+     */
+    topStoryIDArray.forEach(async element => {
+        console.log(element, typeof(element))
+/*         try {
+            const article = await fetch(`https://hacker-news.firebaseio.com/v0/item/${element}.json?print=pretty`)
+            const articleData = await article.json()
+            console.log(articleData)
+            //allTheMovies = [...allTopStories, ...pageData.Search]
+        }
+        catch (err) {
+            console.error(err)
+        } */
+    })
+    return allTopStories
+    //return allTheMovies.filter(movie => (movie.Type === 'movie' && movie.Poster !== 'N/A'))
 }
