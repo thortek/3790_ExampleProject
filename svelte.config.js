d151399a225b6168bca5b13d776dedeef59dcf32
Thor Anderson <ta.anderson@gmail.com>
2022-10-01 22:20:24 -0600

    Still trying to build to work on AWS

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
-import {adapter} from 'sveltekit-adapter-aws'
-import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
 	kit: {
-		adapter: adapter({
-      autoDeploy: true,
-    })
-	},
-
+		adapter: adapter()
+	}
 }
 
 export default config;
