4202691be63695784a2865252bd397fc1e925293
Thor Anderson <ta.anderson@gmail.com>
2022-10-01 20:05:01 -0600

    Package update.  Adapter config to AWS host

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+    prerender: {
+      default: true
+    }
 	},
 
-};
+}
 
 export default config;
