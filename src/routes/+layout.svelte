13ef11b583cc801dba076dfc170fefaf559a535e
Thor Anderson <ta.anderson@gmail.com>
2022-10-01 21:15:46 -0600

    Fixed prerender issue based on Aug 23 PR

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,19 @@
 <script>
 	import '../app.css';
 	import Header from '../lib/components/header/Header.svelte';
+
+	export const csr = false;
+	export const prerender = true;
 </script>
 
 <div data-theme="light" class="h-screen">
-<Header />
+	<Header />
 
-<slot />
+	<slot />
 
-<footer class="footer footer-center p-4 bg-base-300 text-base-content sticky top-[100vh]">
-	<div>
-		<p>Copyright © 2022 Thor Anderson Designs</p>
-	</div>
-</footer>
+	<footer class="footer footer-center p-4 bg-base-300 text-base-content sticky top-[100vh]">
+		<div>
+			<p>Copyright © 2022 Thor Anderson Designs</p>
+		</div>
+	</footer>
 </div>
